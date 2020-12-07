700cbb72ddbd50beb6a26de70b9dd0bf0ecf25e5
vuducbinh88 <57880013+vuducbinh88@users.noreply.github.com>
2020-12-08 00:47:22 +0700

    update image

--- src/main/java/com/example/demo/controller/InsertController.java
+++ src/main/java/com/example/demo/controller/InsertController.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,10 +27,11 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
+import com.example.demo.dao.Service;
 import com.example.demo.dao.Staff;
 import com.example.demo.dao.User;
 import com.example.demo.dto.AddStaffDTO;
+import com.example.demo.service.ServiceRepository;
 import com.example.demo.service.StaffRepository;
 import com.example.demo.service.User2Repository;
 @Controller
@@ -39,6 +41,8 @@ public class InsertController {
 	private StaffRepository staffRepo;
 	@Autowired
 	private User2Repository user2Repo;
+	@Autowired
+	private ServiceRepository ser1Repo;
 	@Autowired
 	private BCryptPasswordEncoder passEnCoder;
 
@@ -102,6 +106,51 @@ public String saveStaff(@ModelAttribute(name="staff") AddStaffDTO staffDto, Mode
 		return "redirect:/dashboard/staff";
 	}
 	
+	@RequestMapping(value="/saveStaff1",method =  RequestMethod.POST)
+	public String saveStaff1(@ModelAttribute(name="staff") Staff staff, Model model,
+				AddStaffDTO staffDto,
+				RedirectAttributes ra,@RequestParam("fileImage") MultipartFile multipartFile) throws IOException, ParseException{
+				
+				String fileName= StringUtils.cleanPath(multipartFile.getOriginalFilename());
+				if(fileName.length()!=0) {
+				staff.setImage(fileName);
+				
+				
+				
+				// save tai khoan 
+				
+//				List<User> userAll = user2Repo.findAll();
+//				User newUser = userAll.get(userAll.size() - 1);
+//				Staff staffSae = new Staff(staffDto.getId_user(), staffDto.getName_staff(), staffDto.getSex(), staffDto.getAddress(), staffDto.getImage(),
+//						staffDto.getEmail(), staffDto.getPhone(), date, 0);
+//				SimpleDateFormat fomater = new SimpleDateFormat("YYYY-MM-dd");
+//				Date date = fomater.parse(staffDto.getDateWorking_Start());
+//				
+//				staff.setDateWorking_Start(date);
+				Staff saveStaff =staffRepo.save(staff);
+				
+				String uploadDir = "/assets/img/" +saveStaff.getId_staff();
+				Path uploadPath =Paths.get(uploadDir);
+				if(!Files.exists(uploadPath)) {
+					Files.createDirectories(uploadPath);
+				}
+				ra.addFlashAttribute("message","oki");
+				
+				try(InputStream inputStream =multipartFile.getInputStream()){
+				Path filePath = uploadPath.resolve(fileName);
+				Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+				}catch(IOException e) {
+					throw new IOException("lỗi"+ fileName);
+				}
+				}else {
+					staff.setImage("default.png");
+					staffRepo.save(staff);
+				}
+				
+				
+			return "redirect:/dashboard/staff";
+		}
+	
 	@RequestMapping(value="/editStaff/{id_staff}")
 	public ModelAndView  editStaff(@PathVariable(name="id_staff") Integer id_staff ) {
 		ModelAndView mav= new ModelAndView("dashboard/editStaff");
@@ -110,7 +159,29 @@ public String saveStaff(@ModelAttribute(name="staff") AddStaffDTO staffDto, Mode
 		return mav;
 	}
 	
+
+	@RequestMapping("/dashboard/services/addServices")
+	public String addService(Model model) {
+		Service service = new Service();
+		model.addAttribute("service",service);
+		return "dashboard/addServices";
+	}
 	
+	@RequestMapping(value="/save",method =  RequestMethod.POST)
+	public String addService(@ModelAttribute("service") Service service) {
+
+		ser1Repo.save(service);
+		
+		return "redirect:/dashboard/services";
+	}
+	
+	@RequestMapping(value="/editService/{id_ser}")
+	public ModelAndView  editService(@PathVariable(name="id_ser") Integer id_ser ) {
+		ModelAndView mav= new ModelAndView("dashboard/editServices");
+		Service service =ser1Repo.getOne(id_ser);
+		mav.addObject("service",service);
+		return mav;
+	}
 
 
 }
